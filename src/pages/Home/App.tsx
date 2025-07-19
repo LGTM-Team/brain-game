c20b67f1c2680407fd18e46d0036ddbeda6e60c0
HyoYoung0829 <gydud7300@naver.com>
2025-07-19 17:21:16 +0900

    Set up base directory structure

--- src/pages/Home/App.tsx
+++ src/pages/Home/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from '@/assets/images/react.svg'
+import neuro from '@/assets/images/Neuro.png'
 import './App.css'
 
 function App() {
@@ -9,8 +9,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://github.com/LGTM-Team/brain-game" target="_blank">
+          <img src={neuro} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
