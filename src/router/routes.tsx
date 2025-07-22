b986d6af0caf1c8f0bf37e93aa8e1889d8e5fd75
Young-mang <fgb9763@gmail.com>
2025-07-22 21:51:37 +0900

    feat(route): routes 배열 정의를 위한 router.tsx 생성

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,7 +1,11 @@
 import GamesPage from "@/pages/Game/GamesPage";
+<<<<<<< HEAD
 import MemorizeNumberCard from "@/pages/Game/MemorizeNumberCard/MemorizeNumberCard";
 import HomePage from "@/pages/Home/HomePage";
 import NotFoundPage from "@/pages/NotFound/NotFoundPage";
+=======
+import HomePage from "@/pages/Home/HomePage";
+>>>>>>> eceec85 (feat(route): routes 배열 정의를 위한 router.tsx 생성)
 import type { JSX } from "react";
 
 interface Route {
@@ -17,11 +21,14 @@ const routes: Route[] = [
     path: "/games",
     element: <GamesPage />,
   },
+<<<<<<< HEAD
   {
     title: "NotFound",
     path: "*",
     element: <NotFoundPage />,
   },
+=======
+>>>>>>> eceec85 (feat(route): routes 배열 정의를 위한 router.tsx 생성)
 ];
 
 export default routes;
