7161257ab699e03dbaf50f0997ea3caf10a941ba
Young-mang <fgb9763@gmail.com>
2025-07-22 21:52:21 +0900

    feat(route): BrowserRouter, useRoutes 적용

--- src/pages/Home/App.tsx
+++ src/pages/Home/App.tsx
@@ -1,35 +1,9 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/images/react.svg'
-import neuro from '@/assets/images/Neuro.png'
-import './components/styles/App.css'
+import { useRoutes } from "react-router-dom";
+import routes from "../../router/routes";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://github.com/LGTM-Team/brain-game" target="_blank">
-          <img src={neuro} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const route = useRoutes(routes!);
+  return route;
 }
 
-export default App
+export default App;
