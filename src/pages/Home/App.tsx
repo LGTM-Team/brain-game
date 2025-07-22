672deb7241862a7cc527c7038d4204b269c217d2
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-07-22 17:54:01 +0900

    chore: media query 설정

--- src/pages/Home/App.tsx
+++ src/pages/Home/App.tsx
@@ -3,10 +3,10 @@ import neuro from "@/assets/images/Neuro.png";
 function App() {
   return (
     <>
-      <div>
+      <div style={{ backgroundColor: `red` }}>
         <h1>뇌하수체</h1>
         <a href="https://github.com/LGTM-Team/brain-game" target="_blank">
-          <img src={neuro} className="logo" alt="Vite logo" />
+          <img src={neuro} alt="Vite logo" />
         </a>
       </div>
     </>
