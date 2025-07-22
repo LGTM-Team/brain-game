82b6c7314eb85f1f841836b50369eeb26a6d2e34
Young-mang <fgb9763@gmail.com>
2025-07-22 21:52:21 +0900

    feat(route): BrowserRouter, useRoutes 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/global.css'
-import App from './pages/Home/App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/global.css";
+import App from "./pages/Home/App";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
--- src/pages/Home/App.tsx
+++ src/pages/Home/App.tsx
@@ -1,18 +1,9 @@
-import neuro from "@/assets/images/Neuro.png";
-import Header from "@/components/Header";
+import { useRoutes } from "react-router-dom";
+import routes from "../../router/routes";
 
 function App() {
-  return (
-    <>
-      <Header title={"뇌하수체"} />
-      <div style={{ backgroundColor: "white" }}>
-        <h1>뇌하수체</h1>
-        <a href="https://github.com/LGTM-Team/brain-game" target="_blank">
-          <img src={neuro} alt="Vite logo" />
-        </a>
-      </div>
-    </>
-  );
+  const route = useRoutes(routes!);
+  return route;
 }
 
 export default App;
