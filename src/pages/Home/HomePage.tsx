f7733f038877a4a9be0e03dcb4057a60c7bda0bb
Young-mang <fgb9763@gmail.com>
2025-07-23 00:00:14 +0900

    chore(header): 파일명 변경 (App.tsx->Homepage.tsx)

---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,18 @@
+import neuro from "@/assets/images/Neuro.png";
+import Header from "@/components/Header";
+
+function HomePage() {
+  return (
+    <>
+      <Header title={"뇌하수체"} />
+      <div style={{ backgroundColor: "white" }}>
+        <h1>뇌하수체</h1>
+        <a href="https://github.com/LGTM-Team/brain-game" target="_blank">
+          <img src={neuro} alt="Vite logo" />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
