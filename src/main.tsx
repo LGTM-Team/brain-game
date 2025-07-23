a7e2a305f6a151c3577165e475c6321309e88e66
Young-mang <fgb9763@gmail.com>
2025-07-23 15:19:00 +0900

    refactor(route): 라우팅 방식 변경BrowserRouter->createBrowserRouter+RouterProvide

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/global.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import routes from "./router/routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={routes} />
   </StrictMode>
 );
