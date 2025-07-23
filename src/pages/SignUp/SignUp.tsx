cbfdd575bf2db5f5836a482665f349089d3428ac
HyoYoung0829 <gydud7300@naver.com>
2025-07-23 16:46:40 +0900

    refactor(SignUp) : 불필요한 useId() 제거.

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -9,13 +9,8 @@ import Input from "@/components/Input";
 import SubmitButton from "@/components/SubmitButton";
 
 function SignUp() {
-  const id = useId();
-  const password = useId();
-  const confirmPassword = useId();
-  const nickname = useId();
-  const gender = useId();
-  const birth = useId();
-  const agree = useId();
+
+
 
   return (
     <>
@@ -25,19 +20,19 @@ function SignUp() {
         <img src={SignUpImg} alt="회원가입 안내 이미지" />
 
         <form>
-          <Input placeholder={"이메일을 입력해주세요. (필수)"} id={id} label={"ID"} />
+          <Input placeholder={"이메일을 입력해주세요. (필수)"} id={"id"} label={"ID"} />
           
-          <Input placeholder={"비밀번호를 입력해주세요. (필수)"} id={password} label={"PW"} />
+          <Input placeholder={"비밀번호를 입력해주세요. (필수)"} id={"password"} label={"PW"} />
           
-          <Input placeholder={"비밀번호를 다시 입력해 주세요. (필수)"} id={confirmPassword} label={"RE-PW"} />
+          <Input placeholder={"비밀번호를 다시 입력해 주세요. (필수)"} id={"confirmPassword"} label={"RE-PW"} />
           
-          <Input placeholder={"닉네임을 입력해 주세요. (필수)"} id={nickname} label={"NICKNAME"} />
+          <Input placeholder={"닉네임을 입력해 주세요. (필수)"} id={"nickname"} label={"NICKNAME"} />
           
           <fieldset className={S.optional}>
             <legend className={S["a11y-hidden"]}>선택 정보</legend>
             <div>
-              <label htmlFor={gender}>GENDER</label>
-              <select id={gender} name="gender">
+              <label htmlFor="gender">GENDER</label>
+              <select id='gender' name="gender">
                 <option value="">성별 선택</option>
                 <option value="male">남성</option>
                 <option value="female">여성</option>
@@ -46,15 +41,15 @@ function SignUp() {
             </div>
 
             <div>
-              <label htmlFor={birth}>BIRTH</label>
-              <input type="date" id={birth} name="birth" />
+              <label htmlFor="birth">BIRTH</label>
+              <input type="date" id="birth" name="birth" />
             </div>
           </fieldset>
 
           <fieldset className={S.agree}>
             <legend className={S["a11y-hidden"]}>약관 동의</legend>
-            <input type="checkbox" id={agree} name="privacy_agree" />
-            <label htmlFor={agree}>개인정보 동의하시겠습니까?</label>
+            <input type="checkbox" id="agree" name="privacy_agree" />
+            <label htmlFor="agree">개인정보 동의하시겠습니까?</label>
           </fieldset>
           
           <SubmitButton label="회원가입"/>
