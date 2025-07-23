b4195588f8f9097644a4beb6cf82537b441a8018
Young-mang <fgb9763@gmail.com>
2025-07-23 11:46:38 +0900

    refactor(route): 중첩 라우트로 개선

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,17 +1,21 @@
+import MainLayout from "@/layouts/MainLayout";
+import GamePage from "@/pages/Games/GamePage";
 import HomePage from "@/pages/Home/HomePage";
 import NoticePage from "@/pages/Notice/NoticePage";
 import QnaPage from "@/pages/Qna/QnaPage";
 import type { JSX } from "react";
 
-interface Route {
-  title: string;
-  path: string;
-  element: JSX.Element;
-}
-//테스트
-
-const routes: Route[] = [
-  { title: "홈", path: "/", element: <HomePage /> },
+const routes = [
+  {
+    title: "메인 레이아웃",
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { title: "홈", path: "", element: <HomePage /> },
+      { title: "게임 바로가기", path: "games", element: <GamePage /> },
+    ],
+  },
+  ,
   { title: "공지사항", path: "/notice", element: <NoticePage /> },
   { title: "고객문의", path: "/qna", element: <QnaPage /> },
 ];
