71598a2131f170d229f02568f1924a5a78b34f39
Young-mang <fgb9763@gmail.com>
2025-07-23 15:23:20 +0900

    refactor(header,navigator): 중복된 header와 navigator 제거

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,18 +1,14 @@
 import neuro from "@/assets/images/Neuro.png";
-import Header from "@/components/Header";
-import Navigation from "@/components/Navigation";
 
 function HomePage() {
   return (
     <>
-      <Header title={"뇌하수체"} />
       <div>
         <h1>뇌하수체</h1>
         <a href="https://github.com/LGTM-Team/brain-game" target="_blank">
           <img src={neuro} alt="Vite logo" />
         </a>
       </div>
-      <Navigation />
     </>
   );
 }
