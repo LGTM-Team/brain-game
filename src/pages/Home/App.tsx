82aa4e2d765671be1a1e29d4277cb105f764a039
Young-mang <fgb9763@gmail.com>
2025-07-23 11:09:02 +0900

    chore(route): pages/Home/App.tsx 제거, App.tsx는 src에 이미 존재하는 파일

--- src/pages/Home/App.tsx
+++
@@ -1,9 +0,0 @@
-import { useRoutes } from "react-router-dom";
-import routes from "../../router/routes";
-
-function App() {
-  const route = useRoutes(routes!);
-  return route;
-}
-
-export default App;
