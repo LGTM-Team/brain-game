3920dff13f762e687fabfcfc7db63c8712607d40
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-07-23 21:19:24 +0900

    ui(Rank): 막대바 최대값을 1등 점수를 기준으로 반영

--- src/components/modal/RankingModal.tsx
+++ src/components/modal/RankingModal.tsx
@@ -2,12 +2,20 @@ import S from "./ranking.module.css";
 import RankItem from "./RankItem";
 import prizeIcon from "@/assets/icons/prize.svg";
 import cancelIcon from "@/assets/icons/cancel.svg";
+import { rankingData } from "./RankData";
+import { useEffect, useState } from "react";
 
 interface Props {
   gameName: string;
 }
 
 function RankingModal({ gameName }: Props) {
+  const [maxScore, setMaxScore] = useState(0);
+
+  useEffect(() => {
+    const topScore = Math.max(...rankingData.map((item) => item.score));
+    setMaxScore(topScore);
+  }, []);
   return (
     <>
       <div className={S.overlay}>
@@ -15,13 +23,13 @@ function RankingModal({ gameName }: Props) {
           <div className={S.cancel}>
             <img src={cancelIcon} alt="닫기" />
           </div>
-
           <div className={S.title}>
             <img src={prizeIcon} alt="랭킹 아이콘" />
             <h2>{gameName} 랭킹</h2>
           </div>
-
-          <RankItem />
+          {rankingData.map((item) => (
+            <RankItem key={item.id} data={item} maxScore={maxScore} />
+          ))}
         </div>
       </div>
     </>
