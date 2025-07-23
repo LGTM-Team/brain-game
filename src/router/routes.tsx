008ff3dc60211423e9c564138090d3c57a5bfc43
Young-mang <fgb9763@gmail.com>
2025-07-23 15:19:00 +0900

    refactor(route): 라우팅 방식 변경BrowserRouter->createBrowserRouter+RouterProvide

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,33 +1,29 @@
-import MainLayout from "@/layouts/MainLayout";
+import App from "@/App";
 import GamePage from "@/pages/Games/GamePage";
 import HomePage from "@/pages/Home/HomePage";
 import NoticePage from "@/pages/Notice/NoticePage";
 import QnaPage from "@/pages/Qna/QnaPage";
-import type { JSX } from "react";
+import { createBrowserRouter } from "react-router-dom";
 
-interface Route {
-  title: string;
-  path: string;
-  element: JSX.Element;
-  children: {
-    title: string;
-    path: string;
-    element: JSX.Element;
-  }[];
-}
-const routes: Route[] = [
+const routes = createBrowserRouter([
   {
-    title: "메인 레이아웃",
     path: "/",
-    element: <MainLayout />,
+    element: <App />,
     children: [
-      { title: "홈", path: "", element: <HomePage /> },
-      { title: "게임 바로가기", path: "games", element: <GamePage /> },
+      { handle: { title: "뇌하수체" }, path: "", element: <HomePage /> },
+      {
+        handle: { title: "게임 바로가기" },
+        path: "games",
+        element: <GamePage />,
+      },
+      {
+        handle: { title: "공지사항" },
+        path: "/notice",
+        element: <NoticePage />,
+      },
+      { handle: { title: "고객문의" }, path: "/qna", element: <QnaPage /> },
     ],
   },
-  ,
-  { title: "공지사항", path: "/notice", element: <NoticePage /> },
-  { title: "고객문의", path: "/qna", element: <QnaPage /> },
-];
+]);
 
 export default routes;
