beaaacfb13e9bbc88bcd75c09c170d1fc25317b1
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-07-23 14:25:31 +0900

    feat(footer):버튼, 아이콘 라우팅 추가 (#17)

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,4 +1,6 @@
 import HomePage from "@/pages/Home/HomePage";
+import NoticePage from "@/pages/Notice/NoticePage";
+import QnaPage from "@/pages/Qna/QnaPage";
 import type { JSX } from "react";
 
 interface Route {
@@ -8,6 +10,10 @@ interface Route {
 }
 //테스트
 
-const routes: Route[] = [{ title: "홈", path: "/", element: <HomePage /> }];
+const routes: Route[] = [
+  { title: "홈", path: "/", element: <HomePage /> },
+  { title: "공지사항", path: "/notice", element: <NoticePage /> },
+  { title: "고객문의", path: "/qna", element: <QnaPage /> },
+];
 
 export default routes;
