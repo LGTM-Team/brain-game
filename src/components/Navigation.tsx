dbdf30318040588237adfdd738058c3e8ce851dc
Young-mang <fgb9763@gmail.com>
2025-07-23 12:00:38 +0900

    feat(route) 게임, 홈화면 라우트 연결

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 import S from "./styles/fixedLayout.module.css";
 import { AuthIcon, GameIcon, HomeIcon } from "./navigationIcons";
+import { AppLink } from "@/router/AppLink";
 
 function Navigation() {
   const [iconColor, setIconColor] = useState("#8c8c8cff");
   return (
     <div className={S.navigation}>
-      <button type="button">
+      <AppLink variant={"tab"} to={"games"}>
         <GameIcon color={iconColor} />
         <p>게임</p>
-      </button>
-      <button className={S.active} type="button">
+      </AppLink>
+      <AppLink className={S.active} variant={"tab"} to={""}>
         <HomeIcon color={iconColor} />
         <p>홈</p>
-      </button>
+      </AppLink>
       <button type="button">
         <AuthIcon color={iconColor} />
         <p>마이페이지</p>
