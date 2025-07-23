ac6feb6845147877a61e4d01712c6f14a812d295
Young-mang <fgb9763@gmail.com>
2025-07-23 11:46:38 +0900

    refactor(route): 중첩 라우트로 개선

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,13 +1,18 @@
+import MainLayout from "@/layouts/MainLayout";
+import GamePage from "@/pages/Games/GamePage";
 import HomePage from "@/pages/Home/HomePage";
 import type { JSX } from "react";
 
-interface Route {
-  title: string;
-  path: string;
-  element: JSX.Element;
-}
-//테스트
-
-const routes: Route[] = [{ title: "홈", path: "/", element: <HomePage /> }];
+const routes = [
+  {
+    title: "메인 레이아웃",
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { title: "홈", path: "", element: <HomePage /> },
+      { title: "게임 바로가기", path: "games", element: <GamePage /> },
+    ],
+  },
+];
 
 export default routes;
