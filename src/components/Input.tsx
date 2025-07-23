024556fc47dbed8d17f450eaa6e5939ef9805a39
HyoYoung0829 <gydud7300@naver.com>
2025-07-23 22:15:02 +0900

    feat(SignUp) : 회원가입 기능 추가 및 공용 컴포넌트 수정(Input.tsx / SubmitButton.tsx)

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,14 +4,17 @@ interface Props {
   placeholder: string;
   label: string;
   id: string;
+  onChange?: (e:React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
+  error?: string;
+  disabled?: boolean;
 }
 
-function Input({ placeholder, id, label }: Props) {
-  const placeholderText = placeholder + "을 입력해 주세요";
+function Input({ placeholder, id, label, onChange, error, disabled }: Props) {
   return (
     <label htmlFor={id} className={S.inputContainer}>
       <p>{label}</p>
-      <input type="text" placeholder={placeholderText} />
+      <input type="text" placeholder={placeholder} onChange={onChange} disabled={disabled}/>
+      {error && <p className={S.errorMessage}>{error}</p>}
     </label>
   );
 }
