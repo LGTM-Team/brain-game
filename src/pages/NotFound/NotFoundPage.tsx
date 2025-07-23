0998c3f9d3aed436776f91615eef75c146ceaf68
Young-mang <fgb9763@gmail.com>
2025-07-23 17:58:50 +0900

    ui(notFound): ui 조정

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,11 +1,11 @@
-import ErrorImg from "@/assets/images/Error_neuro.svg";
+import errorImg from "@/assets/images/error_neuro.svg";
 import S from "./NotFoundPage.module.css";
 
 function NotFoundPage() {
   return (
     <div className={S.container}>
       <div>페이지를 찾을 수 없습니다.</div>
-      <img src={ErrorImg} alt="에러 페이지 안내 이미지" />
+      <img src={errorImg} alt="에러 페이지 안내 이미지" />
     </div>
   );
 }
