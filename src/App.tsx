07c2a1df894a87ac9692f941ecc9911f1ce046ea
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-07-24 15:00:49 +0900

    refactor(App): main 태그에 글로벌 css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
       <Header title={currentTitle} />
-      <main>
+      <main className="container">
         <Outlet />
       </main>
       <Navigation />
