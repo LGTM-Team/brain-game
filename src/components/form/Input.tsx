5595a86bbc24f7ad79fcbdc32435ca0db456b4a9
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-07-24 12:07:36 +0900

    refactor(Form): input, submitbutton 폴더 이동 및 import 경로 변경

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -1,10 +1,12 @@
-import S from "./styles/form.module.css";
+import S from "../styles/form.module.css";
 
 interface Props {
   placeholder: string;
   label: string;
   id: string;
-  onChange?: (e:React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
+  onChange?: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => void;
   error?: string;
   disabled?: boolean;
 }
@@ -13,7 +15,12 @@ function Input({ placeholder, id, label, onChange, error, disabled }: Props) {
   return (
     <label htmlFor={id} className={S.inputContainer}>
       <p>{label}</p>
-      <input type="text" placeholder={placeholder} onChange={onChange} disabled={disabled}/>
+      <input
+        type="text"
+        placeholder={placeholder}
+        onChange={onChange}
+        disabled={disabled}
+      />
       {error && <p className={S.errorMessage}>{error}</p>}
     </label>
   );
--- src/components/form/SubmitButton.tsx
+++ src/components/form/SubmitButton.tsx
@@ -1,10 +1,16 @@
-import S from "./styles/form.module.css";
+import S from "../styles/form.module.css";
+
 interface Props {
   label: string;
   type?: "button" | "submit" | "reset";
   disabled?: boolean;
 }
+
 function SubmitButton({ label, type = "button", disabled = false }: Props) {
-  return <button type={type} className={S.submitButton} disabled={disabled}>{label}</button>;
+  return (
+    <button type={type} className={S.submitButton} disabled={disabled}>
+      {label}
+    </button>
+  );
 }
 export default SubmitButton;
