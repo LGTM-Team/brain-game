0cd3b780270bec21e377de8026ce7a1c430875e4
HyoYoung0829 <gydud7300@naver.com>
2025-07-26 16:40:11 +0900

    ui(FindAccount) : 계정찾기 페이지 디자인

--- src/pages/FindAccount/FindAccount.tsx
+++ src/pages/FindAccount/FindAccount.tsx
@@ -1,9 +1,38 @@
+import Input from '@/components/form/Input';
+import S from './FindAccount.module.css';
+import img from '@/assets/images/findAccountImg.svg'
+import SubmitButton from '@/components/form/SubmitButton';
+
 function FindAccount() {
 
 
-  
+
   return (
-    <div>FindAccount</div>
+    <main className={S.container}>
+      <img src={img} alt="" />
+      <Input 
+        type="email"
+        placeholder="이메일을 입력해주세요." 
+        label="ID 찾기" 
+        id="id"      
+      />
+
+      <Input 
+        type="password"
+        placeholder="새로운 비밀번호를 입력해주세요." 
+        label="Password 재설정" 
+        id="password"      
+      />
+
+      <Input 
+        type="password"
+        placeholder="다시 새로운 비밀번호를 입력해주세요." 
+        label="PW 확인" 
+        id="confirmPassword"      
+      />
+
+      <SubmitButton label='다음'/>
+    </main>
   )
 }
 export default FindAccount
