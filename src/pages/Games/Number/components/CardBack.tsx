7d946dc494f238f1a70b0ab6fbf8a29bfff47265
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-07-28 00:08:48 +0900

    refactor(numbers): NumberPlayPage 파일, 컴포넌트 이름 변경

--- src/pages/Games/Number/components/CardBack.tsx
+++ src/pages/Games/Number/components/CardBack.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import S from "./Card.module.css";
 import { getRandomImage } from "@/utils/getRandom";
 import neuro from "@/assets/images/run_neuro_cloud.svg";
@@ -9,26 +10,52 @@ interface Props {
     width: string;
     height: string;
   };
+  setUserAnswer: React.Dispatch<React.SetStateAction<number[]>>;
 }
 
-function CardBack({ randomNumberList, gridSize, cellSize }: Props) {
-  const imageUrl = getRandomImage() ?? neuro;
-  console.log("gridSize", gridSize);
+function CardBack({
+  randomNumberList,
+  gridSize,
+  cellSize,
+  setUserAnswer,
+}: Props) {
+  const [clickedCardList, setClickedCardList] = useState<boolean[]>(
+    Array(randomNumberList.length).fill(false)
+  );
+
+  const handleClick = (item: number, idx: number) => {
+    //이미 클릭된 카드
+    if (clickedCardList[idx]) return;
+
+    setClickedCardList((prev) => {
+      const newClicked = [...prev];
+      newClicked[idx] = true;
+      return newClicked;
+    });
+    setUserAnswer((prev) => [...prev, item]);
+  };
+
   return (
     <>
       <div
         className={S.grid}
         style={{ gridTemplateColumns: `repeat(${gridSize}, 1fr)` }}
       >
-        {randomNumberList?.map((item, idx) => (
-          <div
-            key={idx}
-            className={`${S.card} ${S.backSide}`}
-            style={{ ...cellSize }}
-          >
-            <img src={imageUrl} />
-          </div>
-        ))}
+        {randomNumberList?.map((item, idx) => {
+          const isClicked = clickedCardList[idx];
+          return (
+            <div
+              key={idx}
+              className={`${S.card} ${isClicked ? S.frontSide : S.backSide}`}
+              style={{ ...cellSize }}
+              onClick={() => {
+                handleClick(item, idx);
+              }}
+            >
+              {isClicked && item !== 0 && <p>{item}</p>}
+            </div>
+          );
+        })}
       </div>
     </>
   );
---
+++ src/pages/Games/Number/components/ShuffleCard.tsx
@@ -0,0 +1,36 @@
+import S from "./Card.module.css";
+import { getRandomImage } from "./getRandomCard";
+import neuro from "@/assets/images/run_neuro_cloud.svg";
+
+interface Props {
+  randomNumberList: number[];
+  gridSize: number;
+  cellSize: {
+    width: string;
+    height: string;
+  };
+}
+
+function ShuffleCard({ randomNumberList, gridSize, cellSize }: Props) {
+  const imageUrl = getRandomImage() ?? neuro;
+
+  return (
+    <>
+      <div
+        className={S.grid}
+        style={{ gridTemplateColumns: `repeat(${gridSize}, 1fr)` }}
+      >
+        {randomNumberList?.map((item, idx) => (
+          <div
+            key={idx}
+            className={`${S.card} ${S.backSide}`}
+            style={{ ...cellSize }}
+          >
+            <img src={imageUrl} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+export default ShuffleCard;
