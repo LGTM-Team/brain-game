98078982369e23521826dc418d0634291a1078de
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-07-28 17:01:00 +0900

    refactor(numbers): numberCard 코드 분리

--- src/pages/Games/Number/components/NumberCard.tsx
+++ src/pages/Games/Number/components/NumberCard.tsx
@@ -2,61 +2,27 @@ import { useEffect, useState } from "react";
 import CardBack from "./CardBack";
 import CardFront from "./CardFront";
 import { motion, AnimatePresence } from "framer-motion";
-import S from "./card.module.css";
-import { useNumberStep } from "../../../../hooks/useNumberStep";
 import ShuffleCard from "./ShuffleCard";
 
 interface Props {
-  round: number;
+  cardStatus: string;
+  gameStep: string;
+  randomNumberList: number[] | null;
+  gridSize: number;
+  setUserAnswer: React.Dispatch<React.SetStateAction<number[]>>;
 }
-// round : 게임 각 단계
-// gameStep : grid가 바뀌는 단계
 
-function NumberCard({ round }: Props) {
-  const [cardSide, setCardSide] = useState<"shuffle" | "front" | "back">(
-    "shuffle"
-  );
-  const [gameStep, setGameStep] = useState<
-    "first" | "second" | "third" | "fourth"
-  >("fourth");
-  const { answer, randomNumberList } = useNumberStep(gameStep);
-  const [userAnswer, setUserAnswer] = useState<number[]>([]);
-  const gridSize = Math.round(Math.sqrt(randomNumberList?.length ?? 1));
-
-  useEffect(() => {
-    const shuffleTimer = setTimeout(() => {
-      setCardSide("front");
-    }, 4000);
-    const gameStater = setTimeout(() => {
-      setCardSide("back");
-    }, 7000);
-    return () => {
-      clearTimeout(shuffleTimer);
-      clearTimeout(gameStater);
-    };
-  }, []);
-
-  // 사용자 입력값 정답 체크
-  useEffect(() => {
-    if (!answer) return;
-    const isCorrectSoFar = userAnswer.every((num, idx) => num === answer[idx]);
-
-    if (!isCorrectSoFar) {
-      console.log("❌ 탈락", userAnswer);
-      // 필요 시 초기화나 shake 효과
-      setUserAnswer([]);
-      return;
-    }
-
-    if (userAnswer.length === answer.length) {
-      console.log("🎉 모두 정답!", userAnswer);
-    }
-  }, [userAnswer]);
-
+function NumberCard({
+  cardStatus,
+  gameStep,
+  setUserAnswer,
+  gridSize,
+  randomNumberList,
+}: Props) {
   return (
     <>
       <AnimatePresence mode="wait">
-        {cardSide === "shuffle" && (
+        {cardStatus === "shuffle" && (
           <motion.div
             key="front"
             initial={{ opacity: 0 }}
@@ -71,7 +37,7 @@ function NumberCard({ round }: Props) {
             />
           </motion.div>
         )}
-        {cardSide === "front" && (
+        {cardStatus === "front" && (
           <motion.div
             key="back"
             initial={{ opacity: 0 }}
@@ -86,7 +52,7 @@ function NumberCard({ round }: Props) {
             />
           </motion.div>
         )}
-        {cardSide === "back" && (
+        {cardStatus === "back" && (
           <motion.div
             key="back"
             initial={{ opacity: 0 }}
--- src/pages/Games/Number/components/NumberPlay.tsx
+++ src/pages/Games/Number/components/NumberPlay.tsx
@@ -4,6 +4,8 @@ import NumberCard from "./NumberCard";
 import CurrentGameScore from "../../components/CurrentGameScore";
 import Timer from "../../components/Timer";
 import StaticTimer from "../../components/StaticTimer";
+import { useNumberStep } from "@/hooks/useNumberStep";
+import { useBonusScore } from "@/hooks/useBonusScore";
 
 interface Props {
   state: "waiting" | "starting" | "playing" | "finish" | "result";
@@ -11,29 +13,76 @@ interface Props {
   onScoreCalculated: (score: number) => void;
   onGameOver: (message: string) => void;
 }
+
+// round : 게임 각 단계
+// gameStep : grid가 바뀌는 단계
+
 function NumberCardPlay({
   state,
   onFinish,
   onScoreCalculated,
   onGameOver,
 }: Props) {
-  const [score, setScore] = useState(0);
-  const [bonusScore, setBonusScore] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
+  const { get, BonusRestart, BonusStart } = useBonusScore(10);
   const [round, setRound] = useState(0);
+  const [cardStatus, setCardStatus] = useState<"shuffle" | "front" | "back">(
+    "shuffle"
+  );
+  const [gameStep, setGameStep] = useState<
+    "first" | "second" | "third" | "fourth"
+  >("first");
+  const { answer, randomNumberList } = useNumberStep(gameStep);
+  const [userAnswer, setUserAnswer] = useState<number[]>([]);
+  const gridSize = Math.round(Math.sqrt(randomNumberList?.length ?? 1));
 
+  // 게임 시작
   useEffect(() => {
-    if (state === "starting") {
-      setScore(0);
-      setRound(0);
+    if (state === "playing") {
+      setRound(1);
+      setCurrentScore(0);
+      BonusStart();
+      setCardStatus("front");
+
+      const gameStater = setTimeout(() => {
+        setCardStatus("back");
+      }, 3000);
+
+      return () => {
+        clearTimeout(gameStater);
+      };
     }
   }, [state]);
 
-  // 정답인지 확인
-  const handleGame = () => {};
+  const handleGame = () => {
+    if (!answer) return;
+    const isCorrectSoFar = userAnswer.every((num, idx) => num === answer[idx]); // 정답인지 확인
+
+    if (!isCorrectSoFar) {
+      setUserAnswer([]);
+      onGameOver("잘못된 순서!");
+      onFinish();
+      return;
+    }
+
+    if (userAnswer.length === answer.length) {
+      console.log("🎉 모두 정답!", userAnswer);
+      const bonus = get();
+      BonusRestart();
+      setRound((prev) => prev + 1);
+    }
+  };
+
+  // 시간이 끝나면 타임 오버
+
+  // 사용자 입력값 정답 체크
+  useEffect(() => {
+    handleGame();
+  }, [userAnswer]);
 
   return (
     <>
-      <CurrentGameScore score={score} />
+      <CurrentGameScore score={currentScore} />
 
       <main className={S.playContainer}>
         <StaticTimer
@@ -43,7 +92,13 @@ function NumberCardPlay({
           key={round}
         />
         <div className={S.cardContainer}>
-          <NumberCard round={round} />
+          <NumberCard
+            cardStatus={cardStatus}
+            gameStep={gameStep}
+            randomNumberList={randomNumberList}
+            gridSize={gridSize}
+            setUserAnswer={setUserAnswer}
+          />
         </div>
       </main>
     </>
