4e22cc7088f30b0abf0e56aa032cb28142304ceb
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-07-31 16:25:40 +0900

    feat(qna):문의 작성 기능 추가

--- src/pages/Qna/write/index.tsx
+++ src/pages/Qna/write/index.tsx
@@ -1,16 +1,30 @@
+import { useRef } from "react";
 import S from "./writeQna.module.css";
 
 export default function QnaWritePage() {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const contentsRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const titleValue = titleRef.current?.value;
+    const contentsValue = contentsRef.current?.value;
+    console.log("제목:", titleValue);
+    console.log("내용:", contentsValue);
+    if (titleRef.current) titleRef.current.value = "";
+    if (contentsRef.current) contentsRef.current.value = "";
+  };
+
   return (
-    <div className={S.container}>
+    <form className={S.container} onSubmit={handleForm}>
       <h5>제목</h5>
-      <input type="text" placeholder="제목을 입력해 주세요" />
+      <input ref={titleRef} type="text" placeholder="제목을 입력해 주세요" />
       <h5>내용</h5>
-      <textarea />
+      <textarea ref={contentsRef} placeholder="상세 내용을 입력해 주세요" />
       <div className={S.spacer}></div>
-      <button>
+      <button type="submit">
         <p>작성 완료</p>
       </button>
-    </div>
+    </form>
   );
 }
