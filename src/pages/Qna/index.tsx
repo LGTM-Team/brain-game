e6dd689edcec2a5c54da1526bf28e7699d26ff27
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-07-31 12:26:57 +0900

    ui(qna): 고객문의 페이지

--- src/pages/Qna/index.tsx
+++ src/pages/Qna/index.tsx
@@ -1,9 +1,20 @@
+import { AppLink } from "@/router/AppLink";
+import PostCard from "../../components/post/PostCard";
 import Question from "./components/Question";
+import S from "./qna.module.css";
+import postIcon from "@/assets/icons/post.svg";
 
 function QnaPage() {
   return (
-    <div>
-      <Question />
+    <div className={S.container}>
+      <PostCard isContact={true} />
+      <PostCard isContact={true} />
+      <PostCard isContact={true} />
+      <div className={S.iconBox}>
+        <AppLink to="/notice" variant="page">
+          <img src={postIcon} alt="고객문의로 이동" />
+        </AppLink>
+      </div>
     </div>
   );
 }
