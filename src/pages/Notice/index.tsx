31874369938a3500e3a48e1c00974d9d97077c4b
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-07-31 12:26:38 +0900

    ui(notice) : 공지사항 페이지

--- src/pages/Notice/index.tsx
+++ src/pages/Notice/index.tsx
@@ -1,11 +1,21 @@
-import Notice from "./components/Notice";
+import { AppLink } from "@/router/AppLink";
+import PostCard from "../../components/post/PostCard";
 import S from "./notice.module.css";
+import qnaIcon from "@/assets/icons/qna.svg";
 
 function NoticePage() {
   const handleToggle = () => {};
   return (
     <div className={S.container}>
-      <Notice />
+      <PostCard isContact={false} />
+      <PostCard isContact={false} />
+      <PostCard isContact={false} />
+
+      <div className={S.iconBox}>
+        <AppLink to="/qna" variant="page">
+          <img src={qnaIcon} alt="고객문의로 이동" />
+        </AppLink>
+      </div>
     </div>
   );
 }
