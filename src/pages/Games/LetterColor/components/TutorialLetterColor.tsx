fc39f13fbbe1db20a1ae78468ec172764c794885
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-08-03 16:36:13 +0900

    feat(tutorial): 색깔을 맞춰라 튜토리얼 추가

--- src/pages/Games/LetterColor/components/TutorialLetterColor.tsx
+++ src/pages/Games/LetterColor/components/TutorialLetterColor.tsx
@@ -1,8 +1,21 @@
+import S from "./styles/tutorialLetterColor.module.css";
+import letterCard from "@/assets/images/game/tutorial/letterColorCard.svg";
+
 function TutorialLetterColor() {
   return (
     <>
-      <div>글자색 맞추기 게임 방법!</div>
-      <p>어쩌구저쩌구 게임방법 설명 알아서들 하쇼 알아서 꾸미셔야합니다.</p>
+      <div className={S.container}>
+        <h3>색깔을 맞춰라</h3>
+        <img src={letterCard} alt="색깔 카드" />
+        <p>
+          주어진 <span>글자의 색깔</span>을 입력해 주세요!
+        </p>
+        <p>
+          <span>오타없이 입력</span>해야 해요!
+        </p>
+        <span>게임 타이머는 점점 빨라집니다.</span>
+        <p>빠르게 생각하고 입력하세요!</p>
+      </div>
     </>
   );
 }
