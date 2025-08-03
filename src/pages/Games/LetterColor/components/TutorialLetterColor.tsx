742a246183a3aaf008297be32b66a5d6a13bd45d
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-08-03 16:38:26 +0900

    refactor(tutorial): 색깔을 맞춰라 튜토리얼 멘트 추가

--- src/pages/Games/LetterColor/components/TutorialLetterColor.tsx
+++ src/pages/Games/LetterColor/components/TutorialLetterColor.tsx
@@ -13,6 +13,7 @@ function TutorialLetterColor() {
         <p>
           <span>오타없이 입력</span>해야 해요!
         </p>
+        <p>주어진 카드의 정답은 초록입니다!</p>
         <span>게임 타이머는 점점 빨라집니다.</span>
         <p>빠르게 생각하고 입력하세요!</p>
       </div>
