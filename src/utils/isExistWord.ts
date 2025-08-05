0f3c44a00b4a41777e09b7eb9c97fa2650d45dc3
HyoYoung0829 <gydud7300@naver.com>
2025-08-05 12:53:31 +0900

    feat(choseong) : 이분 탐색 로직 추가

--- src/utils/isExistWord.ts
+++ src/utils/isExistWord.ts
@@ -1,7 +1,23 @@
 import wordList from '@/data/wordList.json';
 
 function isExistWord(answer: string): boolean {
-  return wordList.includes(answer);
+  let left = 0;
+  let right = wordList.length - 1;
+  
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midWord = wordList[mid];
+    
+    if (midWord === answer) {
+      return true;
+    } else if (midWord < answer) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  
+  return false;
 }
 
 export default isExistWord;
